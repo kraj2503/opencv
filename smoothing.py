84c9c9ba664b2433466d22809907a67a4bc44516
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2022-08-30 23:48:29 +0530

    blurring

---
+++ smoothing.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+
+img = cv.imread('opencv\photos\pboston.jpg')
+cv.imshow('ptest',img)
+
+#  averageing
+
+ 
+
+cv.waitKey(0)
--- splitmerge.py
+++ splitmerge.py
@@ -1,5 +1,30 @@
 import cv2 as cv
-
+import numpy as np
 
 img = cv.imread('opencv\photos\pboston.jpg')
 cv.imshow('ptest',img)
+blank = np.zeros(img.shape[:2],dtype ='uint8')
+
+
+b,g,r = cv.split(img)
+
+blue = cv.merge([b,blank,blank])
+green = cv.merge([blank,g,blank])
+red = cv.merge([blank,blank,r])
+
+cv.imshow('blue',blue)
+cv.imshow('green',green)
+cv.imshow('red',red)
+
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+
+merged = cv.merge([b,g,r])
+cv.imshow('MergedImage', merged)
+
+
+
+cv.waitKey(0)
