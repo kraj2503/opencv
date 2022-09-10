0cd816c01fb6762e5703e74b34ba4d477d561cbf
kraj2503 <73212876+kraj2503@users.noreply.github.com>
2022-09-11 00:04:04 +0530

    Update contours.py

--- contours.py
+++ contours.py
@@ -14,6 +14,8 @@ grey = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 canny= cv.Canny(img,125,175)
 cv.imshow('canny',canny)
 
+
+
 ret,thres =cv.threshold(grey,128,255,cv.THRESH_BINARY)
 
 contours, hierarchies = cv.findContours(thres,cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
